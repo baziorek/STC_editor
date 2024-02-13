a30a460cf4bbf2f7561e45e0d8d7be1b502a8979
Grzegorz Bazior (Yodiss PSA) <g.bazior@yodiss.pl>
2024-02-13 08:48:05 +0100

    Added cheetsheet for body of document

--- main.cpp
+++ main.cpp
@@ -14,6 +14,6 @@ int main(int argc, char *argv[])
  *
  * TODO:
  * 1. Consider using syntax analizer: https://github.com/westes/flex https://stackoverflow.com/questions/2656809/how-do-you-implement-syntax-highlighting
- * 2. Maybe tags gray, normal text black?
+ * 2. Maybe tags gray, normal text black, or tags smaller?
  * 3. *Style sheet instead of style of each diff
  **/
