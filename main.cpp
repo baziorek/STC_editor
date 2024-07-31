3ea40d6c6ce75bb04250f295177a35c4f7211788
Grzegorz Bazior (AGH) <bazior@agh.edu.pl>
2024-07-31 16:33:46 +0200

    Moved UI-connected files to ui directory

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "mainwindow.h"
+#include "ui/mainwindow.h"
 
 #include <QApplication>
 
@@ -23,5 +23,5 @@ int main(int argc, char *argv[])
  * 7. *Images preview
  * 8. Tables creator
  * 9. Better close event https://stackoverflow.com/questions/10417914/handling-exit-without-saving-in-qt https://forum.qt.io/topic/31194/the-right-way-to-do-something-before-closing-the-program-closeevent-abouttoquit-or-the-destructor/2
- * 10. Syntax highlinkk of Cpp & python: QCXXHighlighter https://github.com/Megaxela/QCodeEditor?tab=readme-ov-file (MIT licence)
+ * 10. Syntax highlinkk of Cpp & python: QCXXHighlighter https://github.com/Megaxela/QCodeEditor (MIT licence)
  **/
