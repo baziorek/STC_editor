2a7a9fac85af643af690d0293366c620cf43ce37
GBazior <gbaz@niepodam.pl>
2025-01-02 00:52:49 +0100

    Added logo from chatgpt

--- main.cpp
+++ main.cpp
@@ -3,9 +3,23 @@
 #include <QApplication>
 
 
+void setUpIcon(QApplication& a)
+{
+    QIcon appIcon(":/resources/icon.png");
+
+    if (appIcon.isNull()) {
+        qWarning() << "Nie udało się załadować ikony aplikacji!";
+    } else {
+        qDebug() << "Ikona aplikacji została poprawnie załadowana.";
+        a.setWindowIcon(appIcon);
+    }
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    setUpIcon(a);
+
     MainWindow w;
     w.show();
     return a.exec();
@@ -25,4 +39,6 @@ int main(int argc, char *argv[])
  * 9. Better close event https://stackoverflow.com/questions/10417914/handling-exit-without-saving-in-qt https://forum.qt.io/topic/31194/the-right-way-to-do-something-before-closing-the-program-closeevent-abouttoquit-or-the-destructor/2
  * 10. Syntax highlinkk of Cpp & python: QCXXHighlighter https://github.com/Megaxela/QCodeEditor (MIT licence)
  * 11. Listen external changes of file
+ * 12. Obrazki na serwerze - podmiana prefixów w linkach
+ * 13. Jak jest jakiś tekst napisany od zera (bez wskazania pliku) to chyba dobrze byłoby zapytać użytkownika czy chce wyjść niezapisując?
  **/
